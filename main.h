ee51d417afa87edbbc287ef910998fdb466bd3a3
Mouad <mouad.hamideddine@gmail.com>
2023-10-12 10:59:41 -0700

    0

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts(char *str);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_strdup(char *str);
+
+#endif
